5facc4a178993c0e1791b6f0933542ce8bc2a142
Wei Wu <drweiwu@outlook.com>
2024-03-02 17:01:10 -0800

    first commit

---
+++ testGemini.py
@@ -0,0 +1,21 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+import PIL.Image
+import os
+
+img = PIL.Image.open('image.jpg')
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro-vision')
+#response = model.generate_content(img)
+#print(response)
+
+prompt = "recommend a three course from this menu"
+response = model.generate_content([prompt, img], stream=True)
+response.resolve()
+print(response)
